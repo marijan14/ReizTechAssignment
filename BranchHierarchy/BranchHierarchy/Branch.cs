d07a918e46ae357862b64e5b230ed2fba573aef9
marijan14 <marijan.gajdov@hotmail.com>
2022-11-07 00:24:45 +0100

    Add files via upload

---
+++ BranchHierarchy/BranchHierarchy/Branch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BranchHierarchy
+{
+    internal class Branch
+    {
+        public Branch()
+        {
+        }
+
+        public Branch(List<Branch> branches)
+        {
+            if (branches.Count == 0)
+            {
+                throw new InvalidOperationException($"{nameof(branches)} cannot be empty");
+            }
+
+            Branches = branches;
+        }
+
+        public List<Branch> Branches { get; set; }
+    }
+}
---
+++ BranchHierarchy/BranchHierarchy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BranchHierarchy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sampleTree = new Branch 
+            { 
+                Branches = new List<Branch>
+                {
+                    new Branch(new List<Branch> { new Branch() }),
+                    new Branch(
+                        new List<Branch>
+                        {
+                            new Branch(new List<Branch> { new Branch() }),
+                            new Branch(
+                                new List<Branch>
+                                {
+                                    new Branch(new List<Branch> { new Branch() }),
+                                    new Branch()
+                                }),
+                            new Branch()
+                        })
+                }
+            };
+
+            var depth = GetTreeDepth(sampleTree);
+            Console.WriteLine($"The depth is: {depth}");
+        }
+
+        private static int GetTreeDepth(Branch tree)
+        {
+            static int GetTreeDepthInternal(List<Branch> branches, int depth = 1)
+            {
+                if (branches is null)
+                {
+                    return depth;
+                }
+
+                depth++;
+                var results = new List<int>();
+                foreach (var branch in branches)
+                {
+                    results.Add(GetTreeDepthInternal(branch.Branches, depth));
+                }
+
+                return results.Max();
+            }
+
+            return GetTreeDepthInternal(tree.Branches);
+        }
+    }
+}
